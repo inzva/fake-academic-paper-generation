7c135d53d3219d06301b6aacf26bc56e7724bbc4
Samet Demir <demir.samet@hotmail.com>
2019-03-21 22:50:13 +0300

    dataset_generation folder renamed

--- dataset_generation/downloader.py
+++ dataset_generation/downloader.py
@@ -20,21 +20,18 @@ if not os.path.exists(DOWNLOAD_FOLDER):
 if not os.path.exists(EXTRACT_FOLDER):
         os.mkdir(EXTRACT_FOLDER)
 
-for i in range(0, nb_papers, 1):
+for i in range(nb_papers):
         paper_link = lines[2*i+1]
         paper_code = paper_link.split("/")[-1]
         paper_source_link = "https://arxiv.org/e-print/" + paper_code
-        try:
-                # Download the file from `paper_source_link` and save it locally under `DOWNLOAD_FOLDER+str(i)+".tar.gz"`:
-                compressed_file_path = DOWNLOAD_FOLDER+str(i)+".tar.gz"
-                with urllib.request.urlopen(paper_source_link) as response, open(compressed_file_path, 'wb') as out_file:
-                        shutil.copyfileobj(response, out_file)
 
-                # Extract from tar the tar file
-                tar = tarfile.open(compressed_file_path)
-                paper_folder_dir = EXTRACT_FOLDER + str(i) + "/"
-                tar.extractall(path=paper_folder_dir)
-                tar.close()
-        except Exception:
-                print("error at paper %g" % (i))
-        print("progress: %g / %g" % (i,nb_papers), end="\r")
+        # Download the file from `paper_source_link` and save it locally under `DOWNLOAD_FOLDER+str(i)+".tar.gz"`:
+        compressed_file_path = DOWNLOAD_FOLDER+str(i)+".tar.gz"
+        with urllib.request.urlopen(paper_source_link) as response, open(compressed_file_path, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+
+        # Extract from tar the tar file
+        tar = tarfile.open(compressed_file_path)
+        paper_folder_dir = EXTRACT_FOLDER + str(i) + "/"
+        tar.extractall(path=paper_folder_dir)
+        tar.close()
